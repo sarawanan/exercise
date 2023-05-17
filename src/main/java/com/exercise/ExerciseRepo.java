54835866e5cb87af4b4616da8da31d2b507af939
Sarawanan <sarawananm@icloud.com>
2023-05-17 11:50:42 +0800

    Update ExerciseRepo.java

--- src/main/java/com/exercise/ExerciseRepo.java
+++ src/main/java/com/exercise/ExerciseRepo.java
@@ -2,8 +2,5 @@ package com.exercise;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ExerciseRepo extends JpaRepository<Exercise, String> {
-    List<Exercise> findExerciseByCode(String code);
 }
